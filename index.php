9b1882ba9e7a7cf97289ed145842cbc78eb5ac9c
sinpaw1216 <112193258+sinpaw1216@users.noreply.github.com>
2022-12-26 12:29:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
  </nav>
 </header>
 <div id="center">
-<div id="center-set"> - By Sindhu CSE
+<div id="center-set"> - By Sindhu CSE BIET
 <div id="signup">
 <div id="signup-st">
 <div align="center">
